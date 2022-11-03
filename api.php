b2d226b0f3fa22cd38d313c921118646a8724d99
Zharick Alejandra Vera Meneses <meneseszharick29@gmail.com>
2022-11-03 11:14:27 -0500

    Ejercicio 3

--- api.php
+++ api.php
@@ -3,28 +3,59 @@
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
 
-    class galeria{
-        public $cod;
-        public $cant;
+    class promedio{
+        public $sumaA;
+        public $canA;
+        public $sumaB;
+        public $canB;
+        public $sumaC;
+        public $canC;
 
-        public function __construct(int $cod, int $cant){
-            $this->cod = $cod;
-            $this->cant = $cant;
+        public function __construct(int $sumaA,int $canA,int $sumaB,int $canB, int $sumaC,int $canC){
+            $this->sumaA = $sumaA;
+            $this->canA = $canA;
+            $this->sumaB = $sumaB;
+            $this->canB = $canB;
+            $this->sumaC = $sumaC;
+            $this->canC = $canC;
         }
-        public function codigos(){
-            $cod = match($this->cod){
-                1 => $cod = 200,
-                2 => $cod = 100,
-                3 => $cod = 80,
-            };
-            return $cod;
-        }
-        public function pagar():string{
-            $cod = $this->codigos();
-            $pagar = $cod * $this->cant;
-            return $pagar;
+
+        public function promedioF():string{
+            $sumaA = $this->sumaA;
+            $canA = $this->canA;
+            $sumaB = $this->sumaB;
+            $canB = $this->canB;
+            $sumaC = $this->sumaC;
+            $canC = $this->canC;
+
+            $canA<1?$proA=0:$proA = $sumaA/$canA;
+            $canB<1?$proB=0:$proB = $sumaB/$canB;
+            $canC<1?$proC=0:$proC = $sumaC/$canC; 
+
+            if($proA>$proB && $proA>$proC){
+                if($proB>$proC){
+                    return "C";
+                } else{
+                    return "B";
+                }
+            } else if($proB>$proA && $proB>$proC){
+                if($proA>$proC){
+                    return "C";
+                } else{
+                    return "A";
+                }
+            } else if($proC>$proA && $proC>$proB){
+                if($proA>$proB){
+                    return "B";
+                } else{
+                    return "A";
+                }
+            } else{
+                return "A, B, C";
+            }
         }
     }
-    $lista = new galeria(cod:$_DATA['cod'], cant:$_DATA['cant']);
-    echo($lista->pagar());
+
+    $lista = new promedio(sumaA:$_DATA[0],canA:$_DATA[1],sumaB:$_DATA[2],canB:$_DATA[3],sumaC:$_DATA[4],canC:$_DATA[5]);
+    echo($lista->promedioF());
 ?>
--- main.js
+++ main.js
@@ -1,17 +1,81 @@
+let sumaA = 0, sumaB = 0, sumaC = 0, canA = 0, canB = 0, canC = 0;
+
+function sumaPro(genero, carrera, edad) {
+    if (genero == "M") {
+        if (carrera == "a") {
+            sumaA = sumaA + edad;
+            canA += 1;
+        } else if (carrera == "b") {
+            sumaB = sumaB + edad;
+            canB += 1;
+        } else {
+            sumaC = sumaC + edad;
+            canC += 1;
+        }
+    } else {
+        null
+    }
+}
+
 addEventListener("DOMContentLoaded", (e) => {
+    document.querySelector("#cant").removeAttribute("disabled");
+    document.querySelector("#cant").addEventListener("keyup", (e) => {
+        let genero = document.querySelector("#genero");
+        let edad = document.querySelector("#edad");
+        let carrera = document.querySelector("#carrera");
+        if (e.target.value > 0) {
+            genero.removeAttribute("disabled");
+            edad.removeAttribute("disabled");
+            carrera.removeAttribute("disabled");
+        } else {
+            genero.disabled = true;
+            edad.disabled = true;
+            carrera.disabled = true;
+        }
+    })
+
+    let con = 1;
+    let lista = [];
     let myform = document.querySelector("#form");
     myform.addEventListener("submit", async (e) => {
         e.preventDefault();
-        let form = e.target;
-        let data = Object.fromEntries(new FormData(form));
-        document.querySelector("#resultado").innerHTML = "";
-        let config = {
-            method: form.method,
-            body: JSON.stringify(data)
-        };
-        let peticion = await fetch(form.action, config);
-        let resultado = await peticion.text();
-        document.querySelector("#resultado").insertAdjacentHTML("beforeend", resultado);
-    })
 
+        let cantidad = document.querySelector("#cant").value;
+        document.querySelector("#cant").disabled = true;
+        if (con < cantidad) {
+            let data = Object.fromEntries(new FormData(form));
+            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
+            document.querySelector("#genero").selectedIndex = 0;
+            document.querySelector("#edad").value = "";
+            document.querySelector("#carrera").selectedIndex = 0;
+            con += 1;
+
+        } else {
+            let form = e.target;
+            let data = Object.fromEntries(new FormData(form));
+            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
+            document.querySelector("#resultado").innerHTML = "";
+            lista.push(sumaA);
+            lista.push(canA);
+            lista.push(sumaB);
+            lista.push(canB);
+            lista.push(sumaC);
+            lista.push(canC);
+
+            let config = {
+                method: form.method,
+                body: JSON.stringify(lista)
+            };
+
+            let peticion = await fetch(form.action, config);
+            let respuesta = await peticion.text();
+            document.querySelector("#resultado").insertAdjacentHTML("beforeend", respuesta);
+            document.querySelector("#genero").selectedIndex = 0;
+            document.querySelector("#edad").value = "";
+            document.querySelector("#carrera").selectedIndex = 0;
+            genero.disabled = true;
+            edad.disabled = true;
+            carrera.disabled = true;
+        }
+    })
 })
