a2b83413485a6503852504b656c02c369605e408
Zhari7 <meneseszharick29@gmail.com>
2022-11-06 18:34:34 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,81 +1,43 @@
-let sumaA = 0, sumaB = 0, sumaC = 0, canA = 0, canB = 0, canC = 0;
-
-function sumaPro(genero, carrera, edad) {
-    if (genero == "M") {
-        if (carrera == "a") {
-            sumaA = sumaA + edad;
-            canA += 1;
-        } else if (carrera == "b") {
-            sumaB = sumaB + edad;
-            canB += 1;
-        } else {
-            sumaC = sumaC + edad;
-            canC += 1;
-        }
-    } else {
-        null
-    }
-}
-
 addEventListener("DOMContentLoaded", (e) => {
-    document.querySelector("#cant").removeAttribute("disabled");
-    document.querySelector("#cant").addEventListener("keyup", (e) => {
-        let genero = document.querySelector("#genero");
-        let edad = document.querySelector("#edad");
-        let carrera = document.querySelector("#carrera");
-        if (e.target.value > 0) {
-            genero.removeAttribute("disabled");
-            edad.removeAttribute("disabled");
-            carrera.removeAttribute("disabled");
-        } else {
-            genero.disabled = true;
-            edad.disabled = true;
-            carrera.disabled = true;
-        }
-    })
-
-    let con = 1;
-    let lista = [];
-    let myform = document.querySelector("#form");
-    myform.addEventListener("submit", async (e) => {
+    let form = document.querySelector("#form");
+    form.addEventListener("submit", async (e) => {
         e.preventDefault();
+        let meses = document.querySelector("#meses").value;
+        let dato = new Date(meses);
+        let year,mes,dia;
+        var Pago = 10;
+        if (!!dato.valueOf()) {
+            year = dato.getFullYear();
+            mes = dato.getMonth() + 1;
+            dia = dato.getDate() + 1;
+        }
 
-        let cantidad = document.querySelector("#cant").value;
-        document.querySelector("#cant").disabled = true;
-        if (con < cantidad) {
-            let data = Object.fromEntries(new FormData(form));
-            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
-            document.querySelector("#genero").selectedIndex = 0;
-            document.querySelector("#edad").value = "";
-            document.querySelector("#carrera").selectedIndex = 0;
-            con += 1;
+        let Semana = ['Domingo', 
+        'Lunes', 
+        'Martes',
+        'Miércoles', 
+        'Jueves', 
+        'Viernes', 
+        'Sábado'];
+        
+        let Mes = ['Enero', 
+        'Febrero', 
+        'Marzo', 
+        'Abril', 
+        'Mayo', 
+        'Junio', 
+        'Julio', 
+        'Agosto', 
+        'Septiembre', 
+        'Octubre', 
+        'Noviembre', 
+        'Diciembre'];
 
-        } else {
-            let form = e.target;
-            let data = Object.fromEntries(new FormData(form));
-            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
-            document.querySelector("#resultado").innerHTML = "";
-            lista.push(sumaA);
-            lista.push(canA);
-            lista.push(sumaB);
-            lista.push(canB);
-            lista.push(sumaC);
-            lista.push(canC);
-
-            let config = {
-                method: form.method,
-                body: JSON.stringify(lista)
-            };
-
-            let peticion = await fetch(form.action, config);
-            let respuesta = await peticion.text();
-            document.querySelector("#resultado").insertAdjacentHTML("beforeend", respuesta);
-            document.querySelector("#genero").selectedIndex = 0;
-            document.querySelector("#edad").value = "";
-            document.querySelector("#carrera").selectedIndex = 0;
-            genero.disabled = true;
-            edad.disabled = true;
-            carrera.disabled = true;
+        for(let i=0;i<20;i++){
+            var indice = new Date(year, mes - 1, 16).getDay();
+            document.querySelector("#res").insertAdjacentHTML("beforeend",(`${Semana[indice]} del ${dia} de ${Mes[new Date(year, mes - 1, dia).getMonth()]} del ${(new Date(year, mes - 1, dia).getMonth()==0) ?++year : year} debe pagar ${Pago}<br>`));
+            Pago = Pago * 2;
+            mes++;
         }
     })
 })
