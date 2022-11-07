b33e77d630084bbb33a9e653b0c7e056da8d9968
Zhari7 <meneseszharick29@gmail.com>
2022-11-06 19:03:47 -0500

    Ejercicio 6

--- api.php
+++ api.php
@@ -3,10 +3,23 @@
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
 
-    class Pago{
-        public function __construct($lista){       
+    class promedio{
+        public $lista;
+
+        public function __construct($lista){
+            $this->lista = $lista;
         }
-        public function Ventas():string{
+
+        public function masa():string{
+            $lista = $this->lista;
+
+            $contador= count($lista);
+            $suma= array_sum($lista);
+
+            return $suma/$contador;
     }
 }
+
+$lista = new promedio($_DATA);
+echo($lista->masa());
 ?>
--- main.js
+++ main.js
@@ -1,49 +1,46 @@
-let ventaMen = 0;
-let ventaMed = 0;
-let ventaSup = 0;
-let canMen = 0;
-let canMed = 0;
-let canSup = 0;
-let ventaTotal = 0;
-
-function sumaVentas(precio){
-    precio <= 500?
-        (ventaMen += precio, canMen ++):
-            precio <= 1000 && precio > 500?
-                (ventaMed += precio, canMed ++ ):
-                ( ventaSup += precio, canSup ++ );
-    ventaTotal = ventaMen + ventaMed + ventaSup;
+function masa(presion,volumen){
+    return presion*volumen;
 }
 
 addEventListener("DOMContentLoaded", (e) => {
     document.querySelector("#cantidad").removeAttribute("disabled");
     document.querySelector("#cantidad").addEventListener("keyup", (e) => {
-        let Precio = document.querySelector("#precio");
+        let presion = document.querySelector("#presion");
+        let volumen = document.querySelector("#volumen");
         if (e.target.value > 0) {
-            Precio.removeAttribute("disabled");
+            presion.removeAttribute("disabled");
+            volumen.removeAttribute("disabled");
         } else {
-            Precio.disabled = true;
+            presion.disabled = true;
+            volumen.disabled = true;
         }
     })
-    let con = 1;
+    let con = 1,lista=[];
     let myform = document.querySelector("#form");
     myform.addEventListener("submit", async (e) => {
         e.preventDefault();
         let cantidad = document.querySelector("#cantidad").value;
-        let Precio = parseInt( document.querySelector("#precio").value);
+        let presion = parseInt( document.querySelector("#presion").value);
+        let volumen = parseInt( document.querySelector("#volumen").value);
         document.querySelector("#cantidad").disabled = true;
         if (con < cantidad) {
-            document.querySelector("#precio").value = "";
+            document.querySelector("#presion").value = "";
+            document.querySelector("#volumen").value = "";
             con += 1;
-            sumaVentas(Precio);
+            lista.push(masa(presion,volumen));
         } else {
-            document.querySelector("#resultado").innerHTML = "";
-            sumaVentas(Precio);
-            document.querySelector("#resultado").insertAdjacentHTML
-                ("beforeend",`La cantidad de ventas superiores a 1000 es: ${canSup} y su Total respectivo sera: ${ventaSup} `+
-                `La cantidad de ventas superiores a 500 y menores a 1000 son ${canMed} y su monto respectivo es ${ventaMed} `+
-                `La cantidad de ventas menores a 500 son ${canMen} y su monto respectivo es ${ventaMen} `+
-                `La venta total es de ${ventaTotal}`);
+            let form = e.target;
+            lista.push(masa(presion,volumen));
+            document.querySelector("#presion").disabled=true;
+            document.querySelector("#volumen").disabled=true;
+            let config = {
+                method: form.method,
+                body: JSON.stringify(lista)
+            };
+
+            let peticion = await fetch(form.action, config);
+            let respuesta = await peticion.text();
+            document.querySelector("#resultado").insertAdjacentHTML("beforeend",respuesta)
         }
     })
 })
